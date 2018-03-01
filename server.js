9378a346c467db1ebcbbc4f4000696b184ce5999
Dan Britton <danielbritton@Atlanticbay.com>
2018-03-01 10:54:10 -0500

    Updated Stuff

--- server.js
+++ server.js
@@ -2,14 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(path.join(__dirname)));
-app.use("/styles", express.static(__dirname));
-app.use("/images", express.static(__dirname + '/images'));
-app.use("/scripts", express.static(__dirname + '/scripts'));
+app.use("public",express.static(path.join(__dirname, "public")));
 
 // viewed at based directory http://localhost:8080/
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname,'views/index.html'));
+  res.sendFile(path.join(__dirname,'public/views/index.html'));
 });
 
 // add other routes below
